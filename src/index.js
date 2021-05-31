53a6ed0c57449a23463977e35c3ec2bfe96d1f7b
Shushan-t <tadevosyanshushan23@gmsil.com>
2021-05-31 08:20:03 +0400

    first commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import './Style/index.css';
+import TodoApp from './Todo/TodoApp';
+//import reportWebVitals from './reportWebVitals';
+import store from './Store/store';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const render = () => {
+  ReactDOM.render(
+    <React.StrictMode>
+      <TodoApp />
+    </React.StrictMode>,
+    document.getElementById('root')
+  );
+}
+
+render();
+store.subscribe(render);
+
+
+
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// reportWebVitals();
